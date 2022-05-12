6eeff3ab1f4fe954ed921c77de78cf4c09f95a27
RoshanDiwedi <sroshan154@gmail.com>
2022-05-12 16:02:08 +0530

    styling changes

--- project-3/script.js
+++ project-3/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-    
-});
+    // $(window).on("scroll",function(){
+    //     if(detectViewPort(".ele1")){
+    //         console.log("lfkk");
+    //     }
+    // })
+
+    // function detectViewPort(selector){
+        $(window).on("scroll", function(){
+            if($(window).scrollTop() + $(window).height() - 100 >= $(".ele1").offset().top){
+              console.log("On viewport");
+            }
+          })
+    // }
+  });
 
